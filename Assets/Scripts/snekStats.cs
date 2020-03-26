707bf88f67fcb927918487a6c0f665862d786a53
Ness <vanessa.chu@mail.mcgill.ca>
2020-03-26 01:04:54 -0400

    moved scripts to new folder. added hunger and feed

---
+++ Assets/Scripts/snekStats.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class snekStats : MonoBehaviour
+{
+    public randomMove rm;
+    private float width, height;
+    public float hunger;
+    private float full = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        hunger = full;
+        width = Screen.width;
+        height = Screen.height;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hunger <= 0)
+        {
+            rm.state = 3;
+        }
+        else
+        {
+            hunger = hunger - Time.deltaTime;
+        }
+    }
+
+    public void angerSnake()
+    {
+        rm.state = 3;
+    }
+
+    public float getFull()
+    {
+        return full;
+    }
+}
